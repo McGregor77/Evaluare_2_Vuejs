3cadf2766f18468be845085c28751ebe5de88c14
Edinac Maxim <57366430+McGregor77@users.noreply.github.com>
2022-04-19 10:40:00 +0300

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <section>
+    <input placeholder="num1" type="number" v-model.number="num1">
+    <select v-model="operand" >
+      <option value="+"> + </option>
+      <option value="-"> - </option>
+      <option value="*"> * </option>
+      <option value="/"> / </option>
+    </select>
+    <input placeholder="num2" type="number" v-model.number="num2">
+    <h2> {{result}} </h2>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operand:""
+    };
+  },
+  computed: {
+    result() {
+      switch(this.operand) {
+        case "+":
+          return this.num1 + this.num2
+        case "-":
+          return this.num1 - this.num2
+        case "*":
+          return this.num1 * this.num2
+        case "/":
+          return this.num1 / this.num2
+        default:
+          return this.num1 + this.num2;
+      }
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+</style>
